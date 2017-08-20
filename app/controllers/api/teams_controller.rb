1904e61803411041ba018db9063f9bfcf571ef51
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-20 16:52:06 -0400

    Set up initial Teams Controller

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -1,23 +1,40 @@
 class Api::TeamsController < ApplicationController
 
   def index
+    @teams = Team.all
+    render "/api/teams/index"
   end
 
   def create
+    @team = Team.new(team_params)
+    if @team.save
+      render :show
+    else
+      render json: @team.errors.full_messages, status: 422
+    end
   end
 
   def destroy
+    @team = Team.find(params[:id])
+    @team.delete
+    render :show
   end
 
   def update
+    @team = Team.find(params[:id])
+    if @team.update_attributes description: params[:team][:description]
+      render :show
+    else
+      render json: @team.errors.full_messages, status: 422
+    end
   end
 
   def activity
+    # TODO: Once history table exists
   end
 
   def users
-    @users = User.joins(:users_teams).where( users_teams: {team_id: 1} ).to_a
-
+    @users = User.joins(:users_teams).where( users_teams: {team_id: current_team.id} ).to_a
     render "/api/teams/users"
   end
 
