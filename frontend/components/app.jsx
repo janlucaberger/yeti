b73ff8af0d072280aafdf3f29e3443cd59f8ce85
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-20 18:21:44 -0400

    Add login form to app for temp testing

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
+import { login } from '../actions/session_actions';
+import LoginForm from './login/login_form'
 
 const App = () =>{
   return(
-    <div>This is the app!</div>
+    <div>This is the app!
+    <LoginForm />
+    </div>
   )
 }
 
-export default App
+
+const mapDispatchToProps = dispatch => {
+  return{
+    login: (user) => dispatch(login(user))
+  }
+}
+export default connect(null, mapDispatchToProps)(App)
