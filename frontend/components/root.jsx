e3e7a66824d11513f007a348fe89808b82676838
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-20 18:22:17 -0400

    Update Provider component to receive store

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,9 +3,9 @@ import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './app';
 
-const Root = () => {
+const Root = ({store}) => {
   return(
-    <Provider>
+    <Provider store={store}>
       <HashRouter>
         <App />
       </HashRouter>
