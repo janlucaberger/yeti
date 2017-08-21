d3dad753a49a94ba8f3433af98a9aebd3cafd210
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-21 17:26:32 -0400

    Bootstrap user to window on page refresh

--- frontend/yeti_app.jsx
+++ frontend/yeti_app.jsx
@@ -4,7 +4,13 @@ import Root from './components/root';
 import configureStore from './store/store'
 
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore()
+  let preloadedState = { session: { current_user: null}}
+  if (window.current_user) {
+    preloadedState.session.current_user = window.current_user
+    delete window.current_user
+  }
+
+  const store = configureStore(preloadedState)
   window.store = store
   const root = document.getElementById('root')
   ReactDOM.render(<Root store={store} />, root)
