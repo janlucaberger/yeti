e3005d84ef2cb8b9c95f7c95cdf6945a07b1e52a
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-23 13:46:33 -0400

    Add projects reducer to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import sessionReducer from './session_reducer';
 import teamsReducer from './teams_reducer';
 import uiReducer from './ui_reducer';
+import projectsReducer from './projects_reducer';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
   teams: teamsReducer,
-  ui: uiReducer
+  ui: uiReducer,
+  projects: projectsReducer
 })
 
 export default rootReducer
--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,3 +1,7 @@
 export const getTeamsArray = state => {
   return Object.values(state.teams);
 }
+
+export const getProjectsArray = state => {
+  return Object.values(state.projects)
+}
