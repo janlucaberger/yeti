0b2b6e6e661c7cc668b67a64cca94de6d0f965f6
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-23 16:57:42 -0400

    Add seed file

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'faker', '1.8.4'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -37,7 +38,6 @@ group :development, :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
-  gem 'faker', '1.8.4'
   gem 'pry'
   gem 'byebug'
   gem 'better_errors'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Team.destroy_all
+User.destroy_all
+Project.destroy_all
+teams = [];
+1.times do
+  teams << Team.create(team_name: Faker::Team.name, description: Faker::LeagueOfLegends.quote )
+end
+
+users = []
+15.times do
+  user = User.create(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name, email:Faker::Internet.safe_email , password: "password")
+  users << user
+  UsersTeams.create(user_id: user.id, team_id: teams.sample.id)
+end
+
+5.times do
+  category = Faker::Hacker.adjective
+  Project.create(team_id: teams.sample.id, title: Faker::Hacker.noun , description:Faker::Hacker.say_something_smart, key: category.slice(0,3), user_id: users.sample.id, category: category)
+end
+
+demo = User.create(first_name: "Test" , last_name: "User", email: "test@user.com" , password: "password")
+UsersTeams.create(user_id: demo.id, team_id: teams.sample.id)
