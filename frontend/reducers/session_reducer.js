92214620e26ad92926b1eef56eb15ff714fa8bd7
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-23 19:37:25 -0400

    Add error receiving for sessions reducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,24 +1,27 @@
 import _ from 'lodash';
 
-import { RECEIVE_CURRENT_USER, REMOVE_CURRENT_USER } from '../actions/session_actions'
+import { RECEIVE_CURRENT_USER, REMOVE_CURRENT_USER, RECEIVE_LOGIN_ERROR } from '../actions/session_actions'
 import { RECEIVE_CURRENT_TEAM } from '../actions/teams/teams_actions'
 
 const defaultState = {
   current_user: null,
   current_team: null,
+  errors: []
 }
 
 const sessionReducer = (state = defaultState, action) => {
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return {
+      return _.merge({}, defaultState, {
         current_user: action.user.current_user,
         current_team: action.user.current_team
-       }
+      })
     case RECEIVE_CURRENT_TEAM:
       return _.merge({}, state, {current_team: action.team})
     case REMOVE_CURRENT_USER:
       return defaultState
+    case RECEIVE_LOGIN_ERROR:
+      return _.merge({}, state, {errors: action.error})
     default:
       return state;
   }
--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -5,11 +5,16 @@ const defaultState = {
     display: false,
     component: null,
     props: null
+  },
+  loading: {
+    display: false,
+    props: null
   }
 }
 
-import { RECEIVE_EMAIL_CHECK, RECEIVE_TEAMNAME_CHECK, SHOW_MODAL, HIDE_MODAL } from '../actions/ui_actions'
+import { RECEIVE_EMAIL_CHECK, RECEIVE_TEAMNAME_CHECK, SHOW_MODAL, HIDE_MODAL, SHOW_LOADING, HIDE_LOADING } from '../actions/ui_actions'
 let modalState;
+let loadingState;
 const uiReducer = (state = defaultState, action) => {
   switch (action.type) {
     case RECEIVE_EMAIL_CHECK:
@@ -17,17 +22,28 @@ const uiReducer = (state = defaultState, action) => {
     case RECEIVE_TEAMNAME_CHECK:
       return { team_name_exists: Boolean(action.team.id) }
     case SHOW_MODAL:
-    modalState = {
-      modal: {
-        display: true,
-        component: action.component,
-        props: action.props
+      modalState = {
+        modal: {
+          display: true,
+          component: action.component,
+          props: action.props
+        }
       }
-    }
-    return _.merge({}, state, modalState)
-  case HIDE_MODAL:
-    modalState = { modal: {display: false} }
-    return _.merge({}, state, modalState)
+      return _.merge({}, state, modalState)
+    case HIDE_MODAL:
+      modalState = { modal: {display: false} }
+      return _.merge({}, state, modalState)
+    case SHOW_LOADING:
+      loadingState = {
+        loading: {
+          display: true,
+          props: action.props
+        }
+      }
+      return _.merge({}, state, loadingState)
+    case HIDE_LOADING:
+      loadingState = { loading: {display: false} }
+      return _.merge({}, state, loadingState)
     default:
       return state;
   }
