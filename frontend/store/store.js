eee3c0f849db2f443c7f54529ab443b1f4295de5
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-23 13:46:56 -0400

    Add redux dev tools to store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers/root_reducer'
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    composeEnhancers(
+      applyMiddleware(thunk, logger)
+    )
   )
 )
 
