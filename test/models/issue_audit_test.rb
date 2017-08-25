c5cd33bcca01198446638c2e62689e09e9243f9e
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-24 23:26:34 -0400

    Add tests for new models

---
+++ test/models/issue_audit_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class IssueAuditTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/priority_type_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PriorityTypeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
