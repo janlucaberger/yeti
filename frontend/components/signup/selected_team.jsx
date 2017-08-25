b1b939f7f7066e0dad7087becf0738b6587f9590
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-24 23:27:03 -0400

    Create selected team component for signup flow

---
+++ frontend/components/signup/selected_team.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const SelectedTeam = ({team}) =>{
+
+  if(team){
+    return(
+      <div className="selected-team-container">
+        <h3>Your Team</h3>
+        <h2>{team.team_name}</h2>
+        <img src={team.avatar_url} width="50px" />
+      </div>
+    )
+  } else {
+    return (
+      <div></div>
+    )
+  }
+
+}
+
+export default SelectedTeam
