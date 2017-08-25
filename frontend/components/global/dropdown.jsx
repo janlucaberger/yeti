3733f4aa260d70e94b606e1fa56cf1a2a05ea826
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-24 23:31:53 -0400

    Create dropdown component

---
+++ frontend/components/global/dropdown.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+
+class Dropdown extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      showOptions: false,
+      current_option: "",
+      current_option_icon: ""
+    }
+    this.showOptions = this.showOptions.bind(this)
+    this.hideOptions = this.hideOptions.bind(this)
+    this.closeOptions = this.closeOptions.bind(this)
+    this.options = this.options.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  componentWillReceiveProps(nextProps){
+    const current_option = nextProps.currentOption
+    this.setState({
+      current_option: current_option[this.props.item],
+      current_option_icon: current_option[this.props.iconKey],
+    })
+  }
+
+  showOptions(){
+    this.setState({ showOptions: true})
+    document.addEventListener("click", this.closeOptions)
+  }
+
+  hideOptions(){
+    this.setState({ showOptions: false})
+  }
+
+  closeOptions(e){
+    if(this.node.contains(e.target)){
+      return;
+    }
+    this.hideOptions()
+  }
+
+  handleClick(id){
+    return (e) => {
+      e.stopPropagation();
+      this.props.updateProperty(this.props.changeKey, id)
+      this.hideOptions()
+    }
+  }
+
+  options(){
+    const mappedoptions = Object.values(this.props.options).map((option) => {
+      return(
+        <li key={option.id} onClick={this.handleClick(option.id)} className="dropdown-option-container">
+          <img height="20px" src={option[this.props.iconKey]} />
+          <span className="dropdown-option-text">{option[this.props.item]}</span>
+        </li>
+      )
+    })
+    if(this.state.showOptions){
+      return(
+        <ul className="dropdown-options">
+          { mappedoptions }
+        </ul>
+      )
+    }
+  }
+
+
+  componentWillUnmount(){
+    document.removeEventListener("click", this.closeOptions)
+  }
+
+  render(){
+    return(
+      <div style={{width: this.props.width}} onFocus={this.showOptions} className="dropdown-container">
+        <div className="form-input-title">{this.props.title}</div>
+        <div>
+          <div className="dropdown-icon-placeholder">
+            <img height="20px" src={this.state.current_option_icon}/>
+          </div>
+          <input
+            ref={ node => {this.node = node}}
+            className="dropdown-input"
+            onClick={this.showOptions}
+            value={this.state.current_option}
+          />
+        </div>
+        { this.options() }
+      </div>
+    )
+  }
+
+}
+
+export default Dropdown
