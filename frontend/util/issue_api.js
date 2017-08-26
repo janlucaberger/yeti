ff89e492d1a39f6c6fbc1cb9baa75f651c70f783
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-25 22:42:15 -0400

    Create ajax requests for issue attachments and history

--- frontend/util/issue_api.js
+++ frontend/util/issue_api.js
@@ -7,10 +7,38 @@ export const fetchIssue = id => {
   })
 }
 
+export const fetchIssueHistory = id => {
+  return $.ajax({
+    method: "GET",
+    url: `/api/issues/${id}/history`
+  })
+}
+
 export const updateIssue = issue => {
   return $.ajax({
     method: "PATCH",
     url: `/api/issues/${issue.id}`,
     data: {issue}
+  })
+}
+
+export const postIssueAttachment = formData => {
+  const id = formData.get("issue[id]")
+  return $.ajax({
+    method: "POST",
+    url: `/api/issues/${id}/attachments`,
+    processData: false,
+    contentType: false,
+    dataType: 'json',
+    data: formData
+  })
+}
+
+export const deleteIssueAttachment = issueAttachment => {
+
+  return $.ajax({
+    method: "DELETE",
+    url: `/api/issues/${issueAttachment.issue_id}/attachments`,
+    data: { attachment: issueAttachment }
   })
 }
