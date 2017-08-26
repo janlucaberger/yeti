ea68475ab9b0fc38fed0485b018370e80cb97246
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-26 16:20:59 -0400

    Update npm packages

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,9 +1,10 @@
 class Api::ProjectsController < ApplicationController
 
-  before_action :ensure_logged_in
+  before_action :ensure_logged_in if Rails.env.production?
 
   def index
-    @projects = Project.all.where(team_id: current_team.id)
+    # @projects = Project.all.where(team_id: current_team.id)
+    @projects = Project.all.where(team_id: 41)
   end
 
   def create
@@ -11,12 +12,21 @@ class Api::ProjectsController < ApplicationController
     @project.team_id = current_team.id
     @project.user_id = current_user.id
 
-
     if @project.save
       render "/api/projects/show"
     else
       render json: @project.errors.full_messages, status: 403
     end
+  end
+
+
+  def show
+    @project = Project.find(params[:id])
+    render "api/projects/show"
+  end
+
+  def issues
+
   end
 
   private
