11d15d292cea7fe1779efe73708a7dcf314b2010
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-25 22:45:12 -0400

    Add button and modal action to create a new issue from global nav

--- frontend/components/navigation/global_nav_container.jsx
+++ frontend/components/navigation/global_nav_container.jsx
@@ -3,25 +3,44 @@ import GlobalNavButton from './global_nav_button';
 import { logout } from '../../actions/session_actions'
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { showModal } from '../../actions/ui_actions';
+import NewIssueForm from '../issues/new_issue_form'
 
-const GlobalNavContainer = ({logout}) => {
-  return(
-    <div className='global-nav-container'>
-      <div className='global-nav-button-container'>
-        <GlobalNavButton link={logout} icon="search" size="1"/>
-        <GlobalNavButton link={""} icon="plus" size="1"/>
+
+
+class GlobalNavContainer extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.createNewIssue = this.createNewIssue.bind(this);
+  }
+
+  createNewIssue(){
+    this.props.showModal(NewIssueForm);
+  }
+
+  render(){
+    return(
+      <div className='global-nav-container'>
+        <div className='global-nav-button-container'>
+          <GlobalNavButton link={this.props.logout} icon="search" size="1"/>
+          <button onClick={() => this.createNewIssue()} >NEW ISSUE</button>
+
+        </div>
+        <div className='global-nav-button-container align-bottom'>
+          <GlobalNavButton link={""} icon="plus" size="1" />
+        </div>
       </div>
-      <div className='global-nav-button-container align-bottom'>
-        <GlobalNavButton link={""} icon="plus" size="1" />
-      </div>
-    </div>
-  )
+    )
+  }
+
 }
 
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    logout: () => dispatch(logout())
+    logout: () => dispatch(logout()),
+    showModal: (component, props) => dispatch(showModal(component, props)),
   }
 }
 
