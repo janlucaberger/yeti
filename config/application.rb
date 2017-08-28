0ef673143959df986521c28f49b3843718ff72f2
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-28 16:19:08 -0400

    Update aws settings

--- config/application.rb
+++ config/application.rb
@@ -10,14 +10,14 @@ module Yeti
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    
+
     config.paperclip_defaults = {
       :storage => :s3,
       :s3_credentials => {
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
-        :s3_region => ENV["s3_region"]
+        :s3_region => "us-east-1"
       }
     }
     # Settings in config/environments/* take precedence over those specified here.
