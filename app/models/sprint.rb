845d118b751f57e93a1826c9a898ac0e9dc6ffd6
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-30 08:49:04 -0400

    Fix model validation for sprints

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,6 +1,6 @@
 class Sprint < ApplicationRecord
 
-  validates :name, :start_date, :end_date, :project, :active, presence: true
+  validates :name, :start_date, :end_date, :project, presence: true
   belongs_to :project
 
 end
